8f5d115c59c70d22bd7aaa5f22c65633cc4e6929
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2020-10-28 19:45:00 +0100

    Update 2.create_script.sh

--- 1.MaSuRCA/2.create_script.sh
+++ 1.MaSuRCA/2.create_script.sh
@@ -1,1 +1,6 @@
+module load Boost
+module load Perl/5.30.2-GCCcore-9.3.0
+cd ~/
+~/Software/MaSuRCA-3.4.2/bin/masurca config.txt
 
+#  REMEMBER EDIT SCRIPT FOR PARALLELIZATION (LINE 62) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
