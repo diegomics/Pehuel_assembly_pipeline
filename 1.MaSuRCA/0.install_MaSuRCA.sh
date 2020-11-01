f0d3b85ddeb993b4a5183d4a2c9880ad42077ad0
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2020-11-01 13:40:35 +0100

    Update 0.install_MaSuRCA.sh

--- 1.MaSuRCA/0.install_MaSuRCA.sh
+++ 1.MaSuRCA/0.install_MaSuRCA.sh
@@ -4,6 +4,8 @@ wget https://github.com/alekseyzimin/masurca/releases/download/v3.4.2/MaSuRCA-3.
 tar -xvzf MaSuRCA-3.4.2.tar.gz
 rm MaSuRCA-3.4.2.tar.gz
 cd MaSuRCA-3.4.2
-module load Boost
-module load Perl/5.30.2-GCCcore-9.3.0
-./install.sh
+module load GCC/8.3.0
+module load GCCcore/8.3.0
+module load Perl/5.30.0-GCCcore-8.3.0
+module load Boost/1.71.0-gompic-2019b
+./install.sh |& tee install_log.txt
