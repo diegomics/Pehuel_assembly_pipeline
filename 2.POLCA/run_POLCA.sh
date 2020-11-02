cb785497ab81f2e475cbcc124ac877db2c280014
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2020-11-02 10:50:11 +0100

    Update run_POLCA.sh

--- 2.POLCA/run_POLCA.sh
+++ 2.POLCA/run_POLCA.sh
@@ -23,4 +23,4 @@ module load Boost/1.71.0-gompic-2019b
 export PATH=~/Software/MaSuRCA-3.4.2/bin:$PATH
 export PATH=~/Software/bwa-0.7.17:$PATH
 
-polca.sh -a ~/Assemblies/MaS/CA/final.genome.scf.fasta -r '~/ILLUMINA_demit.1.fq.gz ~/ILLUMINA_demit.2.fq.gz' -t 24 -m 1G
+bash polca.sh -a ~/Assemblies/MaS/CA/final.genome.scf.fasta -r '~/ILLUMINA_demit.1.fq.gz ~/ILLUMINA_demit.2.fq.gz' -t 24 -m 1G
