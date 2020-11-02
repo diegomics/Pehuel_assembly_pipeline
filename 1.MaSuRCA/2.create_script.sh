bc7d6208b462175f9c8096a3ed23bd74e5ea8a02
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2020-11-02 19:30:26 +0100

    Update 2.create_script.sh

--- 1.MaSuRCA/2.create_script.sh
+++ 1.MaSuRCA/2.create_script.sh
@@ -1,5 +1,9 @@
-module load Boost
-module load Perl/5.30.2-GCCcore-9.3.0
+module load GCC/8.3.0
+module load GCCcore/8.3.0
+module load Perl/5.30.0-GCCcore-8.3.0
+module load Boost/1.71.0-gompic-2019b
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 cd ~/
 ~/Software/MaSuRCA-3.4.2/bin/masurca config.txt
 
