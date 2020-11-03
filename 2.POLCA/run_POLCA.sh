977489af790f81447538cc0635c8e7d01d0cc552
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2020-11-03 19:32:01 +0100

    Update run_POLCA.sh

--- 2.POLCA/run_POLCA.sh
+++ 2.POLCA/run_POLCA.sh
@@ -12,15 +12,17 @@
 ##SBATCH -w b001
 #SBATCH --qos=standard
 #SBATCH --cpus-per-task=24
-#SBATCH --mem-per-cpu=1G
-#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=24:00:00
 
 module load GCC/8.3.0
 module load GCCcore/8.3.0
 module load Perl/5.30.0-GCCcore-8.3.0
 module load Boost/1.71.0-gompic-2019b
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 export PATH=~/Software/MaSuRCA-3.4.2/bin:$PATH
 export PATH=~/Software/bwa-0.7.17:$PATH
 
-bash polca.sh -a ~/Assemblies/MaS/CA/final.genome.scf.fasta -r '~/ILLUMINA_demit.1.fq.gz ~/ILLUMINA_demit.2.fq.gz' -t 24 -m 1G
+bash polca.sh -a ~/Assemblies/MaS/CA/final.genome.scf.fasta -r '~/ILLUMINA_demit.1.fq.gz ~/ILLUMINA_demit.2.fq.gz' -t 24 -m 2G
