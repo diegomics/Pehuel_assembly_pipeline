6570ad78e5b36654d0f4ad5d3132076b38191609
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2021-11-14 17:26:12 +0100

    Update 2.create_script.sh

--- 1.MaSuRCA/2.create_script.sh
+++ 1.MaSuRCA/2.create_script.sh
@@ -4,7 +4,5 @@ module load Perl/5.32.0-GCCcore-10.2.0
 module load Boost/1.74.0-GCC-10.2.0
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-cd ~/
-~/Software/MaSuRCA-3.4.2/bin/masurca config.txt
-
-#  REMEMBER EDIT SCRIPT FOR PARALLELIZATION (LINE 62) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+cd ../Condor_project/Pehuel/intermediates/masurca/PE
+../Software/MaSuRCA-3.4.2/bin/masurca config.txt
