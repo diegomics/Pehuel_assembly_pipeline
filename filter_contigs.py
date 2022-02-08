af7fa270036fe9c7854f1de6ed8de025d49bdddb
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-02-08 16:36:54 +0100

    Update filter_contigs.py

--- filter_contigs.py
+++ filter_contigs.py
@@ -4,7 +4,7 @@ import sys
 CONTIGS_FILE = open(sys.argv[1], "r")
 PARSED_CONTIGS_FILE = SeqIO.parse(CONTIGS_FILE, "fasta")
 counter = 0
-for CONTIG in PARSED_CONTIGS_FILED:
+for CONTIG in PARSED_CONTIGS_FILE:
 	if ( len(CONTIG.seq) >= int(sys.argv[2]) ) and ( ( float( CONTIG.seq.count("N") ) / len(CONTIG.seq) ) * 100 ) <= int(sys.argv[3]):
 		print(">" + CONTIG.id)
 		print(CONTIG.seq)
