195d98a686c7ea7c73719efeb701db060e752742
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-05-24 15:54:55 +0200

    Update 3.create_script.sh

--- 2.MaSuRCA/3.create_script.sh
+++ 2.MaSuRCA/3.create_script.sh
@@ -4,5 +4,5 @@ module load Perl/5.32.0-GCCcore-10.2.0
 module load Boost/1.74.0-GCC-10.2.0
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-cd ../Condor_project/Pehuel/intermediates/masurca/PE
-../Software/MaSuRCA-4.0.6/bin/masurca config.txt
+cd ~/Condor_project/Pehuel/intermediates/masurca/PE
+~/Software/MaSuRCA-4.0.6/bin/masurca config.txt
