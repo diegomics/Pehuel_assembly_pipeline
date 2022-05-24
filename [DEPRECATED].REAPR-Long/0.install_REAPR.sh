3d9e9a14b523d9dfb01647e31348f19e6f36b5d9
D.N. De Panis <9466288+diegomics@users.noreply.github.com>
2022-05-24 15:58:57 +0200

    Delete [DEPRECATED].REAPR-Long directory

--- [DEPRECATED].REAPR-Long/0.install_REAPR.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~/Software
-conda create -n REAPR_env
-conda activate REAPR_env
-conda install -c anaconda networkx
-git clone --recursive https://github.com/mdriller/masterTool.git
-cd masterTool/scripts/minimap2/
-make
